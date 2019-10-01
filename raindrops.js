1c6cc9d9500ee91886f99c5450ecb8875de6ddfc
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-01 09:21:23 +0300

    answered warm up

--- raindrops.js
+++ raindrops.js
@@ -1,2 +1,15 @@
 // Your Answer below
+let a = prompt("enter a number");
+let result = "";
 
+if (a % 3 == 0) {
+    result="Pling"
+} else if (a%5 == 0) {
+    result = "Plang";
+} else if (a % 7 == 0) {
+    result = "Plong";
+} else {
+    toString(a)
+    result = a
+}
+console.log(result)
